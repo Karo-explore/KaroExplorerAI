6dce124aece209ef51d711c21df7d475a9e2990f
Karo-explore <kachan59amirhosseini@gmail.com>
2026-08-06 05:03:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("KaroExplorerAI v0.4");
+const pi =
+"31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+const button = document.getElementById("searchButton");
+
+button.addEventListener("click", search);
+
+function search(){
+
+    const word = document.getElementById("word").value.trim();
+    const result = document.getElementById("result");
+
+    if(word === ""){
+        result.innerHTML = "Please enter digits.";
+        return;
+    }
+
+    const position = pi.indexOf(word);
+
+    if(position !== -1){
+        result.innerHTML =
+        "✅ Found!<br><br>Position: " + position;
+    }else{
+        result.innerHTML =
+        "❌ Not Found";
+    }
+
+}
